538807a5cb547a648257827c4573fe9f2cd182c0
Clément <clementperrette@eaton.com>
2021-01-22 08:53:17 +0000

    Add the test

---
+++ src/dlopen-test.cpp
@@ -0,0 +1,20 @@
+#include <dirent.h> 
+#include <dlfcn.h>
+
+#include <iostream>
+#include <memory>
+
+int main (int argc, char *argv[]) {
+
+    if(argc != 1) {
+        std::cerr << "Usage: " << argv[0] << " libpath" << std::endl;
+    }
+    //try to load the lib
+    std::shared_ptr<void> m_spLib = std::shared_ptr<void>(dlopen(argv[1], RTLD_NOW), [] (void * ptr) { if(ptr) dlclose(ptr);});
+    if (!m_spLib) {
+        std::cerr << "Cannot load lib " << argv[1] << ": " << dlerror() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
